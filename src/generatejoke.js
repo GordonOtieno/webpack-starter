87fe3ffcccb324eca054aba614736f2ce3617f83
GordonOtieno <otienogordon95@gmail.com>
2022-08-09 13:38:56 -0400

    add webpack files

---
+++ src/generatejoke.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+function generatejoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  axios.get('https://icanhazdadjoke.com', config).then((res) => {
+    document.getElementById('joke').innerHTML = res.data.joke
+  })
+}
+
+export default generatejoke
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import  generatejoke from './generatejoke';
+import './styles/main.scss';
+import laughing from './assets/test.svg'
+
+const laughImg = document.getElementById('testImg')
+laughImg.src = laughing
+
+
+const jokeBtn = document.getElementById('jokeBtn')
+jokeBtn.addEventListener('click', generatejoke)
+
+generatejoke()
+
+// https://github.com/bradtraversy/webpack-starter/blob/main/webpack.config.js
